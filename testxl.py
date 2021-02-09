27920c2b3efb6bcd5777ee7ee7a8fd986d3b7023
Yee <dyee@air.org>
2021-02-09 16:19:12 -0500

    First working version

--- testxl.py
+++ testxl.py
@@ -1,65 +1,80 @@
 # %%
 from typing import Optional, Union
 import openpyxl as xl
+import pandas as pd
+import numpy as np
+
+# %%
+
+DATAPATH = 'U:/ESSIN Task 14/Mapping Report/2019/Standard Method/02_Mapping Tool Design/Data Source/SourceTable_States/SAS/Updated data/Long Files/Updated 0208'
+OUTPATH = 'U:/ESSIN Task 14/Mapping Report/2019/Standard Method/06_Analysis and Results/Darrick/Web Tables'
+dta = dict()
+for gr in 'G4', 'G8':
+    dta[gr] = pd.read_excel(f'{DATAPATH}/{gr}_long.xlsx')
+    for col in 'nse', 'nse_se', 'nse_re':
+        dta[gr][col] = pd.to_numeric(dta[gr][col], errors='coerce')
+
+df = dta['G4'].append(dta['G8'])
+del dta
+# %%
 
 
-class Row:  # pylint: disable=inherit-non-class
-    """Simple class for NSE row display values
-    """
+def display_row(year: int,
+                nse: float = np.nan,
+                nse_se: float = np.nan,
+                nse_re: float = np.nan):
+    nse_nan = np.isnan(nse)
+    return tuple((
+        year,
+        '-' if nse_nan else nse,
+        '†' if nse_nan else (
+            '-' if np.isnan(nse_se) else nse_se),
+        '†' if nse_nan else (
+            '-' if np.isnan(nse_re) else nse_re),
+        '!' if nse_re > 0.5 else ''
+    ))
+# %%
 
-    def __init__(self, year: int,
-                 nse: Optional[float] = None,
-                 nse_se: Optional[float] = None,
-                 nse_re: Optional[float] = None) -> None:
-        self.year = year
-        self.nse: Union[float, str] = nse or '-'
-        self.nse_se: Union[float, str] = nse_se or (
-            '†' if nse is None else '-')
-        self.nse_re: Union[float, str] = nse_re or (
-            '†' if nse is None else '-')
-
-    def astuple(self) -> tuple[int, float, str]:
-        return tuple((self.year, self.nse, self.nse_se, self.nse_re))
-
-    def __getitem__(self, idx):
-        return self.astuple()[idx]
-
-    def __iter__(self):
-        return iter(self.astuple())
-
-    def __repr__(self) -> str:
-        return f"Row(year={self.year}, nse={self.nse}, nse_se={self.nse_se}, nse_re={self.nse_re})"
 
 # %%
 
 
-wb = xl.load_workbook('source.xlsx')
-
-sheets = {sg: wb[sg] for sg in ('G4', 'G8')}
-
-data = {
-    'state': 'Vermont',
-    'R4': [Row(2005, None, None, None),
-           Row(2007, 263, 1.4, None),
-           Row(2009, 253.502772375101, 0.638233566056301, 0.2665152040405)]
+COL_OFFSETS = {
+    'R': 3,
+    'M': 7
 }
 
-for sg, ws in sheets.items():
-    ws['B5'].value = ws['B5'].value.replace('[STATE_NAME]', data['state'])
-    datarows = data['R4']
+WB = xl.load_workbook('source.xlsx')
 
-    for row in range(len(data['R4'])):
-        for col in range(4):
-            cell = ws.cell(row+9, col+2)
-            cell.value = datarows[row][col]
+sheets = {sg: WB[sg] for sg in ('G4', 'G8')}
+
+STATES = tuple(dict.fromkeys(df['state']))
 
 
-wb.save('mywb.xlsx')
+#%%
+
+for state in STATES:
+
+    for grade in '4', '8':
+        ws = sheets['G'+grade]
+        ws['B5'].value = ws['B5'].value.replace('[STATE_NAME]', state)
+
+        data = df[(df['subjgrade'].str.endswith(grade)) & (df['state'] == state)]
+
+        for i, year in enumerate(dict.fromkeys(data['year'])):
+            ws.cell(i+9, 2).value = year
+
+        for subj in 'R', 'M':
+
+            j = COL_OFFSETS[subj]
+
+            for i, row in enumerate(data[data['subjgrade']==subj+grade].loc[:, 'year':'nse_re'].itertuples()):
+                outrow = display_row(*row[1:])
+                for col in range(len(outrow[1:])):
+                    cell = ws.cell(i+9, col+j)
+                    cell.value = outrow[col+1]
+
+
+        WB.save(f'{OUTPATH}/{state}.xlsx')
+
 # %%
-# –	†	†
-# 263 	1.4 	–
-# 254 	0.6 	0.3
-# 248 	0.6 	0.2
-# 245 	0.5 	0.2
-# 266 	0.8 	0.2
-# 272 	1.5 	0.4
